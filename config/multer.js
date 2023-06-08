fc041e02ec9e9ed60ae3884d829b00649c447953
ACER <nafasyarahma15@gmail.com>
2023-06-08 23:41:00 +0700

    configure image storage on gcs

--- config/multer.js
+++ config/multer.js
@@ -1,52 +1,61 @@
 import multer from 'multer';
 import { Storage } from '@google-cloud/storage';
 
-const storage = new Storage({
+export const storage = new Storage({
   projectId: 'capstone-opet',
+  keyFilename: './config/keyFile.json'
 });
 
-const bucketName = 'bucket_for_pet_photos';
+export const bucket = storage.bucket('bucket_for_pet_photos');
 
-const multerStorage = multer.memoryStorage();
 
-const upload = multer({
-  storage: multerStorage,
-  limits: {
-    fileSize: 10 * 1024 * 1024, // batasan ukuran file (opsional)
-  },
-});
 
-export const fileUpload = upload.single('file');
+// const storage = new Storage({
+//   projectId: 'capstone-opet',
+// });
 
-export const uploadFile = async (req, res, next) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ message: 'No file uploaded.' });
-    }
+// const bucketName = 'bucket_for_pet_photos';
 
-    const bucket = storage.bucket(bucketName);
-    const fileData = req.file;
+// const multerStorage = multer.memoryStorage();
 
-    const blob = bucket.file(fileData.originalname);
+// const upload = multer({
+//   storage: multerStorage,
+//   limits: {
+//     fileSize: 10 * 1024 * 1024, // batasan ukuran file (opsional)
+//   },
+// });
 
-    const blobStream = blob.createWriteStream({
-      resumable: false,
-      contentType: fileData.mimetype,
-    });
+// export const fileUpload = upload.single('file');
 
-    blobStream.on('error', (err) => {
-      console.log(err);
-      next(err);
-    });
+// export const uploadFile = async (req, res, next) => {
+//   try {
+//     if (!req.file) {
+//       return res.status(400).json({ message: 'No file uploaded.' });
+//     }
 
-    blobStream.on('finish', () => {
-      const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
-      res.status(200).json({ url: publicUrl });
-    });
+//     const bucket = storage.bucket(bucketName);
+//     const fileData = req.file;
 
-    blobStream.end(fileData.buffer);
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
-};
+//     const blob = bucket.file(fileData.originalname);
+
+//     const blobStream = blob.createWriteStream({
+//       resumable: false,
+//       contentType: fileData.mimetype,
+//     });
+
+//     blobStream.on('error', (err) => {
+//       console.log(err);
+//       next(err);
+//     });
+
+//     blobStream.on('finish', () => {
+//       const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
+//       res.status(200).json({ url: publicUrl });
+//     });
+
+//     blobStream.end(fileData.buffer);
+//   } catch (err) {
+//     console.log(err);
+//     next(err);
+//   }
+// };
