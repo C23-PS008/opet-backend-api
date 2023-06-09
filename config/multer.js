eb3e74fa38b9bb74538650a145164c6a21a75252
kusumayasa <kusumayasa@student.unud.ac.id>
2023-06-09 11:07:26 +0800

    update multer.js

--- config/multer.js
+++ config/multer.js
@@ -2,7 +2,7 @@ import { Storage } from '@google-cloud/storage';
 
 export const storage = new Storage({
   projectId: 'capstone-opet',
-  keyFilename: './config/kserviceaccountkey.json'
+  keyFilename: './serviceaccountkey.json'
 });
 
 export const bucket = storage.bucket('bucket_for_pet_photos');
